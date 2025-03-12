39d4ae307112651a8e3bed6ce4889eaae427eed4
nitinmehra09 <nitinmehra9983@gmail.com>
2025-03-12 19:34:30 +0530

    more programs in OOps added

---
+++ OOPs/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    // properties
+    int salary;
+    String name;
+    // methods 
+    int getSalary(){
+        return this.salary;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setName(String n){
+        this.name = n;
+    }
+}
